757346ae63508e24f7603ee082b048f4c8b68ef7
Petr Heinz <noobos@gmail.com>
2018-11-12 22:28:44 +0100

    YamlFixCommand: path to file provided via argument

--- src/Command/YamlFixCommand.php
+++ src/Command/YamlFixCommand.php
@@ -8,6 +8,7 @@ use PetrHeinz\YamlTools\File;
 use PetrHeinz\YamlTools\ItemCollection;
 use PhpCsFixer\Differ\UnifiedDiffer;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -20,10 +21,13 @@ final class YamlFixCommand extends Command
 
     public const EXIT_CODE_SUCCESS = 0;
 
+    private const OPTION_PATH = 'path';
+
     protected function configure(): void
     {
         $this
             ->setName(self::COMMAND_NAME)
+            ->addArgument(self::OPTION_PATH, InputArgument::REQUIRED)
             ->setDescription('Fixes YAML config files.');
     }
 
@@ -31,10 +35,17 @@ final class YamlFixCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $contents = file_get_contents(__DIR__ . '/../../example.yml');
+        $path = $input->getArgument(self::OPTION_PATH);
+
+        if (!is_string($path)) {
+            $io->error(sprintf('The path should have been a string, %s provided instead.', gettype($path)));
+            return self::EXIT_CODE_ERROR;
+        }
+
+        $contents = file_get_contents($path);
 
         if ($contents === false) {
-            $io->error('The file is not readable.');
+            $io->error(sprintf('The file "%s" is not readable.', $path));
             return self::EXIT_CODE_ERROR;
         }
 
